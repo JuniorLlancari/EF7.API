fca012fca3ee66afb1d274e9170ac4c5174ac5f8
JuniorLlancari <juniorllancariv@gmail.com>
2023-05-11 14:58:27 -0500

    update - pagina swagger por defecto

--- EF7.API/Program.cs
+++ EF7.API/Program.cs
@@ -18,11 +18,15 @@ builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment()) { }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nombre de tu API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
